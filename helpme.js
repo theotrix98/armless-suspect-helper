2363e42e1aea6dc945c235604226b65e29012a54
theotrix98 <officiallytheatric@gmail.com>
2026-05-23 19:29:47 -0500

    Create helpme.js

---
+++ helpme.js
@@ -0,0 +1,55 @@
+// THIS SHIT IS SO ASS!!
+// I AM PASTING STUFF HERE SO I DONT LOSE IT
+// EVEN THOUGH I WILL PROBABLY SCRAP ALL OF THIS THE MINUTE I LEARN TO GET GOOD
+// GOD IS DEAD AND I DIDNT KILL HER. SHE LOOKED AT MY SPAGHETTI CODE AND KILLED HERSELF
+
+const base = "Base game map";
+const limited = "Event-only map";
+
+const mapName = {
+    mapString: "Map Name; Map description",
+    mapType: base,
+    evidenceAmount: 3,
+    possibleEvidence: {
+        coldBlooded: "",
+        clean: "",
+        food: "",
+        fingerprints: "",
+        hiddenEvidence: ""
+    },
+    suspects: {
+        suspectOne: {
+            name: "John Doe",
+            blurb: "that guy from uhh forsaken or wait no die of- no. fuck. where the hell is he from",
+            susTraits: {
+                isColdBlooded: true,
+                isClean: false,
+                likesFood: false,
+                leavesFingerPrints: true,
+                hidesEvidence: true
+            }
+        },
+        suspectTwo: {
+            name: "Jane Doe",
+            blurb: "THERE'S ANOTHER ONE??? FUCK!!",
+            susTraits: {
+                isColdBlooded: false,
+                isClean: true,
+                likesFood: true,
+                leavesFingerPrints: true,
+                hidesEvidence: false
+            }
+        },
+        suspectThree {
+            name: "Funni easter egg",
+            blurb: "Insert niche reference here hahahaha im so funny somebody kill me",
+            susTraits: {
+                isColdBlooded: false,
+                isClean: false,
+                likesFood: true,
+                leavesFingerPrints: true,
+                hidesEvidence: true
+            }
+        }
+    }
+}
